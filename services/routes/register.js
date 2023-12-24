c7a2c85aeaf64b93e6ae5ad6124149d96effe230
Mark Masoumi <masoumi.mark@outlook.com>
2023-12-24 18:17:48 -0500

    removed old commented code from services/routes/register.js

--- services/routes/register.js
+++ services/routes/register.js
@@ -96,11 +96,11 @@ router.post('/register', async (req, res) => {
             const createdUserFirebase = await auth.getAuth().createUser({ email: newUser.email, password: newUser.password });
 
             // Create new user on Postgresql:
-            const createdUser = await requests.addUser(newUser.firstName, newUser.lastName, newUser.email, createdUserFirebase.uid);
+            const createdUserPG = await requests.addUser(newUser.firstName, newUser.lastName, newUser.email, createdUserFirebase.uid);
 
             // If a user is successfully created, create a new Cart for that user in the DB.
-            if(createdUser){
-                requests.addCart(createdUser.id);                
+            if(createdUserPG){
+                requests.addCart(createdUserPG.id);                
                 res.status(201).json("Registration was successful");
             }            
         }
@@ -111,56 +111,3 @@ router.post('/register', async (req, res) => {
         res.status(500).json(`Registration failed - error: ${error}`);
     }
 });
-
-
-
-
-
-// router.post('/register', async (req, res) => {
-//     try {
-//         // Get registration info from the request body                        
-//         const newUser = { firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, password: req.body.password }
-                
-//         // Check if the request body has any missing data
-//         if (!newUser.firstName || !newUser.lastName || !newUser.email || !newUser.password) return res.status(400).json("Registration failed. Required info is missing.");
-                
-//         // Validate user input                
-//         const validationArray = [];
-//         validationArray.push(validator.isAlpha(newUser.firstName));
-//         validationArray.push(validator.isLength(newUser.firstName, { min: 1, max: 50 }));
-//         validationArray.push(validator.isAlpha(newUser.lastName));
-//         validationArray.push(validator.isLength(newUser.lastName, { min: 1, max: 50 }));
-//         validationArray.push(validator.isEmail(newUser.email));                
-//         validationArray.push(validator.isStrongPassword(newUser.password, { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 }));
-                                
-//         // Check if any element in array is false
-//         const foundInvalidInput = validationArray.some((e) => { return e === false });        
-//         if (foundInvalidInput) return res.status(400).json("Registration failed. Required info is invalid.");
-                
-//         // Check if a user with that email already exists in the db
-//         const userCheck = await requests.getUserByEmail(newUser.email);
-
-//         if (!userCheck) {
-//             // Encrypt the user's password:
-//             // 1. Generate salt with 10 Salt Rounds
-//             const salt = await bcrypt.genSalt(10);
-
-//             // 2. Hash password
-//             const hashedPassword = await bcrypt.hash(newUser.password, salt);
-                        
-//             // 3. Create new user:
-//             const createdUser = await requests.addUser(newUser.firstName, newUser.lastName, newUser.email, hashedPassword);
-
-//             // If a user is successfully created, create a new Cart for that user in the DB.
-//             if(createdUser){
-//                 requests.addCart(createdUser.id);                
-//                 res.status(201).json("Registration was successful");
-//             }            
-//         }
-//         else{
-//             res.status(409).json("Registration failed. User with that email address already exists.");                        
-//         }
-//     } catch (error) {        
-//         res.status(500).json(`Registration failed - error: ${error}`);
-//     }
-// });
